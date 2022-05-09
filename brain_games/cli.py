809ed004da272a63978f3e9e1ad910fe562e25f6
Kverde <kverde@yandex.ru>
2022-05-09 17:23:50 +0500

    Step 5

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,6 +4,11 @@ import prompt
 
 
 def welcome_user():
-    """Ask user name and great him."""
+    """Ask user name and great him.
+
+    Returns:
+        Username.
+    """
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')  # noqa: WPS421
+    return user_name
